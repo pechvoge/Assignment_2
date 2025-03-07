506a887c43543ecd4cc825f7be0da5be61698ea1
luukdijkman <l.dijkman@student.utwente.nl>
2025-03-07 09:15:51 +0100

    Script is running but exits with Alarm Clock

--- periodic_thread.cpp
+++ periodic_thread.cpp
@@ -3,13 +3,23 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
 
-static sigset_t sigset;
+static sigset_t sig_set;
 
 void wait_next_activation(void)
 {
     int sig;
-    sigwait(&sigset, &sig);
+    printf("Waiting for next activation\n");
+    int res = sigwait(&sig_set, &sig);
+    if (res != 0)
+    {
+        perror("sigwait failed");
+        exit(1);
+    }
+    printf("Activated\n");
 }
 
 int start_periodic_timer(uint64_t offset, int period)
@@ -21,15 +31,14 @@ int start_periodic_timer(uint64_t offset, int period)
     int err_creation;
     const int signal = SIGALRM;
    
-    //start periodic timer
     t.it_value.tv_sec = offset / 1000000;
-    t.it_value.tv_nsec = (offset % 1000)*1000;
+    t.it_value.tv_nsec = (offset % 1000000)*1000;
     t.it_interval.tv_sec = period / 1000000;
     t.it_interval.tv_nsec = (period % 1000000)*1000;
 
-    sigemptyset(&sigset);
-    sigaddset(&sigset, signal);
-    sigprocmask(SIG_BLOCK, &sigset, NULL);
+    sigemptyset(&sig_set);
+    sigaddset(&sig_set, signal);
+    sigprocmask(SIG_BLOCK, &sig_set, NULL);
 
     memset(&sigev, 0, sizeof(struct sigevent));
     sigev.sigev_notify = SIGEV_SIGNAL;
@@ -41,7 +50,15 @@ int start_periodic_timer(uint64_t offset, int period)
         perror("Timer creation failed.");
         return -1;
     }
-    return timer_settime(timerid, 0, &t, NULL);
+    int err_settime;
+    err_settime = timer_settime(timerid, 0, &t, NULL);
+    if (err_settime < 0)
+    {
+        perror("Setting timer failed.");
+        return -1;
+    }
+    printf("Timer started\n");
+    return 0;
 }
 
 void* periodicThread(void *arg)
@@ -50,6 +67,7 @@ void* periodicThread(void *arg)
     uint64_t offset = 1000; //1ms
     int period = 1000; //1ms
 
+    //start periodic timer
     int err_timer;
     err_timer = start_periodic_timer(offset, period);
     if (err_timer < 0)
@@ -59,7 +77,7 @@ void* periodicThread(void *arg)
 
     while (1)
     {
-        //job body
+        for (int i = 0; i < 1000; i++){}
         wait_next_activation();
     }
 }
