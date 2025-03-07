812bb63c16512095edf2c7bfedbe278ae8312b95
luukdijkman <l.dijkman@student.utwente.nl>
2025-03-07 11:39:33 +0100

    Added comments to working periodic thread file.

--- periodic_thread.cpp
+++ periodic_thread.cpp
@@ -12,61 +12,56 @@
 
 static sigset_t sig_set;
 
-bool isPrime(int n) 
-{ 
-    // Corner case 
-    if (n <= 1) 
-        return false; 
-  
-    // Check from 2 to n-1 
-    for (int i = 2; i < n; i++) 
-        if (n % i == 0) 
-            return false; 
-  
-    return true; 
-} 
-
 void wait_next_activation(void)
 {
+    // Waits for a pending asynchronous signal 
     int sig;
     int res = sigwait(&sig_set, &sig);
     if (res != 0)
     {
-        perror("sigwait failed");
+        perror("Sigwait failed");
         exit(1);
     }
 }
 
 int start_periodic_timer(uint64_t offset, int period)
 {
-    //initialization
+    // Initialization
     struct itimerspec t;
     struct sigevent sigev;
     timer_t timerid;
     int err_creation;
+    int err_settime;
     const int signal = SIGALRM;
    
+    // Sets the timer values
     t.it_value.tv_sec = offset / 1000000;
     t.it_value.tv_nsec = (offset % 1000000)*1000;
     t.it_interval.tv_sec = period / 1000000;
     t.it_interval.tv_nsec = (period % 1000000)*1000;
 
+    // Creates an empty set and adds a signal to it and blocks this signal  
     sigemptyset(&sig_set);
     sigaddset(&sig_set, signal);
     sigprocmask(SIG_BLOCK, &sig_set, NULL);
 
+    // Initializes the sigevent structure
     memset(&sigev, 0, sizeof(struct sigevent));    
+
+    // Setup the sigevent structure to notify the thread with current ID
     sigev.sigev_notify = SIGEV_THREAD_ID;
     sigev.sigev_signo = signal;
     sigev._sigev_un._tid = syscall(SYS_gettid);
 
-    err_creation = timer_create(CLOCK_REALTIME, &sigev, &timerid);
+    // Creates the timer with a monotonic clock
+    err_creation = timer_create(CLOCK_MONOTONIC, &sigev, &timerid);
     if (err_creation < 0)
     {
         perror("Timer creation failed.");
         return -1;
     }
-    int err_settime;
+
+    // Sets the timer
     err_settime = timer_settime(timerid, 0, &t, NULL);
     if (err_settime < 0)
     {
@@ -79,17 +74,18 @@ int start_periodic_timer(uint64_t offset, int period)
 
 void* periodicThread(void *arg)
 {
-    //initialization
-    uint64_t offset = 1000; //1ms
-    int period = 1000; //1ms
+    // Initialization
+    uint64_t offset = 1000; //1 ms
+    int period = 1000; //1 ms
+    struct timespec start, wait, end;
     std::ofstream timeLog("timeLog.txt");
     if(!timeLog.is_open())
     {
         perror("Time log could not be opened.");
     }
-    struct timespec start, wait, end;
+    
 
-    //start periodic timer
+    // Starts the periodic timer with an offset and period of 1ms
     int err_timer;
     err_timer = start_periodic_timer(offset, period);
     if (err_timer < 0)
@@ -99,22 +95,29 @@ void* periodicThread(void *arg)
 
     while (1)
     {
-        clock_gettime(CLOCK_REALTIME, &start);
+        // Measures time before the computation
+        clock_gettime(CLOCK_MONOTONIC, &start);
         for (int i = 0; i < 100000; i++){}
 
-        clock_gettime(CLOCK_REALTIME, &wait);
+        // Measures time after the computation and before the sigwait function
+        clock_gettime(CLOCK_MONOTONIC, &wait);
 
         wait_next_activation();
-        clock_gettime(CLOCK_REALTIME, &end);
+        // Measures time after the sigwait function
+        clock_gettime(CLOCK_MONOTONIC, &end);
 
+        // Writes the computation time and the waiting time to the file
         timeLog << (wait.tv_sec - start.tv_sec) * 1000000 + (wait.tv_nsec - start.tv_nsec) / 1000 << "\t"; //ms
         timeLog << (end.tv_sec - wait.tv_sec) * 1000000 + (end.tv_nsec - wait.tv_nsec) / 1000 << "\n"; //ms
     }
 }
 
 int main(){
+    // Creates a new thread
     pthread_t thread;
     pthread_create(&thread, NULL, periodicThread, NULL);
+
+    // Looks whether the new thread is finished
     pthread_join(thread, NULL);
     return 0;
 }
