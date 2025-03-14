199ba50560dae9ac11047518077160c07da80ebf
Luuk Dijkman <l.dijkman@student.utwente.nl>
2025-03-14 12:43:19 +0100

    Bad results of test session

--- periodic_thread_RT.cpp
+++ periodic_thread_RT.cpp
@@ -14,6 +14,7 @@
 #include <evl/timer.h>
 #include <fcntl.h>
 #include <evl/proxy.h>
+#include <evl/sched.h>
 #include <sys/mman.h>
 #include <sched.h>
 
@@ -27,29 +28,24 @@ static sigset_t sig_set;
 void wait_next_activation(void)
 {
     // Waits until the next activation
-    unsigned long overruns;
-    int err_wait = evl_wait_period(&overruns);
-    if (err_wait < 0)
-    {
-        perror("Waiting for next activation failed.");
-    }
+    
 }
 
 int start_periodic_timer(uint64_t offset, int period)
 {
     // Initialization
     struct itimerspec t;
+    struct evl_sched_attrs attrs;
+    attrs.sched_policy = SCHED_FIFO;
+    attrs.sched_priority = 10;
     timer_t timerid;
-    int err_creation, err_settime, err_attach, err_core;
-
-    ktime_t koffset = ktime_set(0, offset*1000); // 1 ms
-    ktime_t kperiod = ktime_set(0, period*1000); // 1 ms
+    int err_creation, err_settime, err_attach, err_core, err_sched;
    
     // // Sets the timer values
-    // t.it_value.tv_sec = offset / 1000000;
-    // t.it_value.tv_nsec = (offset % 1000000)*1000;
-    // t.it_interval.tv_sec = period / 1000000;
-    // t.it_interval.tv_nsec = (period % 1000000)*1000;
+    t.it_value.tv_sec = offset / 1000000;
+    t.it_value.tv_nsec = (offset % 1000000)*1000;
+    t.it_interval.tv_sec = period / 1000000;
+    t.it_interval.tv_nsec = (period % 1000000)*1000;
 
     // Set CPU affinity to core 1 before attaching to EVL core
     cpu_set_t cpu_set;
@@ -62,36 +58,36 @@ int start_periodic_timer(uint64_t offset, int period)
         return -1;
     }
 
-    int err_period = evl_setperiod(&evl_mono_clock,koffset,kperiod);
-    if (err_period < 0)
+    // Attaches the current thread to the EVL core
+    err_attach = evl_attach_self("periodic_thread_RT");
+    if (err_attach < 0)
     {
-        perror("Setting period failed.");
+        perror("Attaching to EVL failed.");
         return -1;
     }
 
-    // // Creates the EVL timer with a monotonic clock
-    // err_creation = evl_new_timer(EVL_CLOCK_MONOTONIC);
-    // if (err_creation < 0)
-    // {
-    //     perror("Timer creation failed.");
-    //     return -1;
-    // }
+    // Creates the EVL timer with a monotonic clock
+    err_creation = evl_new_timer(EVL_CLOCK_MONOTONIC);
+    if (err_creation < 0)
+    {
+        fprintf(stderr, "Timer creation failed: %s\n", strerror(err_creation));
+        return -1;
+    }
 
-    // // Attaches the current thread to the EVL core
-    // err_attach = evl_attach_self("periodic_thread_RT");
-    // if (err_attach < 0)
-    // {
-    //     perror("Attaching to EVL failed.");
-    //     return -1;
-    // }
+    err_sched = evl_set_schedattr(err_attach, &attrs);
+    if (err_sched != 0)
+    {
+        fprintf(stderr, "Scheduling failed: %s\n", strerror(err_sched));
+        return -1;
+    }
 
-    // // Sets the EVL timer
-    // err_settime = evl_set_timer(err_creation, &t, NULL);
-    // if (err_settime < 0)
-    // {
-    //     perror("Setting timer failed.");
-    //     return -1;
-    // }
+    // Sets the EVL timer
+    err_settime = evl_set_timer(err_creation, &t, NULL);
+    if (err_settime < 0)
+    {
+        perror("Setting timer failed.");
+        return -1;
+    }
     printf("Timer started\n");
     return 0;
 }
@@ -104,15 +100,14 @@ void* periodicThread(void *arg)
     size_t buffer_size = 1024;
     struct timespec start, wait, end;
     int write_fd, proxy_fd, computation_time, waiting_time;
-    char *log_msg;
-    
-    write_fd = open("./timeLog.txt", O_WRONLY|O_CREAT);
-    if(write_fd < 0)
-        {
-            perror("Time log could not be opened.");
-        }
+
+    // write_fd = open("./timeLog.txt", O_WRONLY|O_CREAT);
+    // if(write_fd < 0)
+    //     {
+    //         perror("Time log could not be opened.");
+    //     }
     // Creates a proxy for the current thread
-    proxy_fd = evl_create_proxy(write_fd,buffer_size,"Realtime proxy");
+    //proxy_fd = evl_new_proxy(write_fd,buffer_size,"Realtime proxy");
     
     // Starts the periodic timer with an offset and period of 1ms
     int err_timer = start_periodic_timer(offset, period);
@@ -135,12 +130,12 @@ void* periodicThread(void *arg)
         evl_read_clock(CLOCK_MONOTONIC, &end);
 
         // Writes the computation time and the waiting time to the file
-        computation_time = (wait.tv_sec - start.tv_sec) * 1000000 + (wait.tv_nsec - start.tv_nsec) / 1000;
-        waiting_time = (end.tv_sec - wait.tv_sec) * 1000000 + (end.tv_nsec - wait.tv_nsec) / 1000;
-        log_msg = createLogMsg(computation_time, waiting_time);
+        // computation_time = (wait.tv_sec - start.tv_sec) * 1000000 + (wait.tv_nsec - start.tv_nsec) / 1000;
+        // waiting_time = (end.tv_sec - wait.tv_sec) * 1000000 + (end.tv_nsec - wait.tv_nsec) / 1000;
+        // char *log_msg = createLogMsg(computation_time, waiting_time);
         
-        oob_write(proxy_fd, log_msg, sizeof(log_msg));
-        oob_write(proxy_fd, log_msg, sizeof(log_msg));
+        // oob_write(proxy_fd, log_msg, sizeof(log_msg));
+        // delete[] log_msg;
     }
     evl_detach_self();
 }
@@ -150,14 +145,18 @@ int main(){
     mlockall(MCL_CURRENT | MCL_FUTURE);
 
     // Sets scheduling policy to Round Robin and ensures EVL is out of band
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
-    pthread_attr_setschedpolicy(&attr, SCHED_RR);
+    // pthread_attr_t attr;
+    // pthread_attr_init(&attr);
+    // pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+    // pthread_attr_setschedpolicy(&attr, SCHED_RR);
     
     // Creates a new thread
     pthread_t thread;
-    pthread_create(&thread, &attr, periodicThread, NULL);
+    int err = pthread_create(&thread, NULL, periodicThread, NULL);
+    if (err != 0) {
+        fprintf(stderr, "Thread creation failed: %s\n", strerror(err));
+        return -1;
+    }
 
     // Looks whether the new thread is finished
     pthread_join(thread, NULL);
