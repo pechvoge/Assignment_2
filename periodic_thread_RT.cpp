b4684526ecac58b1580e31377614924d82034b85
luukdijkman <l.dijkman@student.utwente.nl>
2025-03-14 10:57:04 +0100

    Added logging functionality

--- periodic_thread_RT.cpp
+++ periodic_thread_RT.cpp
@@ -17,6 +17,10 @@
 #include <sys/mman.h>
 #include <sched.h>
 
+int amountDigitsDeterminer(int number);
+char* intToASCII(int number);
+int getCharArrayLength(const char* array);
+char* createLogMsg(int computeTime, int waitTime);
 
 static sigset_t sig_set;
 
@@ -100,7 +104,7 @@ void* periodicThread(void *arg)
     size_t buffer_size = 1024;
     struct timespec start, wait, end;
     int write_fd, proxy_fd, computation_time, waiting_time;
-    const char *log_msg;
+    char *log_msg;
     
     write_fd = open("./timeLog.txt", O_WRONLY|O_CREAT);
     if(write_fd < 0)
@@ -133,6 +137,7 @@ void* periodicThread(void *arg)
         // Writes the computation time and the waiting time to the file
         computation_time = (wait.tv_sec - start.tv_sec) * 1000000 + (wait.tv_nsec - start.tv_nsec) / 1000;
         waiting_time = (end.tv_sec - wait.tv_sec) * 1000000 + (end.tv_nsec - wait.tv_nsec) / 1000;
+        log_msg = createLogMsg(computation_time, waiting_time);
         
         oob_write(proxy_fd, log_msg, sizeof(log_msg));
         oob_write(proxy_fd, log_msg, sizeof(log_msg));
@@ -157,4 +162,71 @@ int main(){
     // Looks whether the new thread is finished
     pthread_join(thread, NULL);
     return 0;
+}
+
+int amountDigitsDeterminer(int number)
+{
+	if (number == 0)
+	{
+		return 1;
+	}
+	
+	int amountDigits = 0;
+	while (number != 0)
+	{
+		number /= 10;
+		amountDigits++;
+	}
+	return amountDigits;
+}
+
+char* intToASCII(int number)
+{
+    const int amountDigits = amountDigitsDeterminer(number);
+    char* myASCIIchar = new char[amountDigits + 1]; // +1 for null terminator
+    for (int i = amountDigits - 1; i >= 0; i--)
+    {
+        myASCIIchar[i] = static_cast<char>((number % 10) + '0');
+        number /= 10;
+    }
+    myASCIIchar[amountDigits] = '\0';
+    return myASCIIchar;
+}
+
+int getCharArrayLength(const char* array)
+{
+    int length = 0;
+    while (array[length] != '\0')
+    {
+        length++;
+    }
+    return length;
+}
+
+char* createLogMsg(int computeTime, int waitTime)
+{
+    char* computeChar = intToASCII(computeTime);
+    int computeCharLength = getCharArrayLength(computeChar);    
+    char* waitChar = intToASCII(waitTime);
+    int waitCharLength = getCharArrayLength(waitChar);
+    
+    // Allocate enough space for both strings, a tab, a newline, and the null terminator
+    char* logMsg = new char[computeCharLength + waitCharLength + 3];
+
+	// Combine both char arrays into log message
+    for (int i = 0; i < computeCharLength; i++)
+    {
+        logMsg[i] = computeChar[i];
+    }
+    logMsg[computeCharLength] = '\t';
+    for (int i = 0; i < waitCharLength; i++)
+    {
+        logMsg[computeCharLength + 1 + i] = waitChar[i];
+    }
+    logMsg[computeCharLength + 1 + waitCharLength] = '\n';
+    logMsg[computeCharLength + 1 + waitCharLength + 1] = '\0';
+    
+    delete[] computeChar;
+    delete[] waitChar;
+    return logMsg;
 }
